b0e14f3f9a31e379256ffbf47bdfd51ad46f0916
nykim <uionu@naver.com>
2022-03-24 10:24:25 +0900

    오류

---
+++ src/main/java/study/StudyApplication.java
@@ -0,0 +1,16 @@
+package study;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/study/controller/WebController.java
@@ -0,0 +1,13 @@
+package study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("index")
+    public String index (){
+        return "index.html";
+    }
+}
